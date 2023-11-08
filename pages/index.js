81d7296e15ce79e5484116b959e11e279d0751c7
Minato Arisato <zlaumari022@yahoo.com>
2023-11-09 00:24:53 +0800

    edit

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Zach Carreon
@@ -86,7 +87,7 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} alt="" />
+              <Image className="ml-12" src={design} width={100} height={100} alt="" />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
@@ -101,7 +102,7 @@ export default function Home() {
               {/* <p className="text-gray-800 py-1">Indesign</p> */}
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} alt=""/>
+              <Image className="ml-12" src={code} width={100} height={100} alt=""/>
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Code your dream project
               </h3>
@@ -113,7 +114,7 @@ export default function Home() {
               {/* <p className="text-gray-800 py-1">Indesign</p> */}
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} alt=""/>
+              <Image className="ml-12" src={consulting} width={100} height={100} alt=""/>
               <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
               <p className="py-2">
                 Are you interested in feedback for your current project? I can
